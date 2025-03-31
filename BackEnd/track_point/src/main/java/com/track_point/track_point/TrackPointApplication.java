a3f7e5b22a69a2ea6be261baf16daa84a33504c5
brudneyuk <brudney.ramosjr@sptech.school>
2025-03-31 19:34:24 -0300

    feat: Adicionando estrutura Spring do backend

---
+++ BackEnd/track_point/src/main/java/com/track_point/track_point/TrackPointApplication.java
@@ -0,0 +1,13 @@
+package com.track_point.track_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackPointApplication.class, args);
+	}
+
+}
---
+++ BackEnd/track_point/src/test/java/com/track_point/track_point/TrackPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track_point.track_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
